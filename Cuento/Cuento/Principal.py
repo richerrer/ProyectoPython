c0142c7007cbc6ee063c04be5de664873cd2cac7
Raul Mira <mirarodriguezraul0812@gmail.com>
2012-12-22 23:59:27 -0500

    modifico la clase principal

--- Cuento/Cuento/Principal.py
+++ Cuento/Cuento/Principal.py
@@ -1,4 +1,5 @@
 import pygame
+import Tkinter as tk
 from pygame.locals import *
 
 class Principal():
@@ -14,34 +15,39 @@ class Principal():
         
     def iniciarCuento(self):
         canal = pygame.mixer.Channel(2)
-        
         sonido = pygame.mixer.Sound("parte00.wav")
-        sonido2 = pygame.mixer.Sound("parte00 Seleccion.wav")
-        sonido3 = pygame.mixer.Sound("parte01 Seleccion.wav")
+        sonido1 = pygame.mixer.Sound("parte00 Seleccion.wav")
+        sonido2 = pygame.mixer.Sound("parte01 Seleccion.wav")
         canal.queue(sonido)
+        canal.queue(sonido1)
+        self.eleccionAcontecimiento(sonido,sonido1,"parte01.wav",canal)
         canal.queue(sonido2)
-        sonido3.play()
-        entrada = self.capturarEntrada()
-        self.eleccionAcontecimiento(entrada, canal)
-    
-    def eleccionAcontecimiento(self,entrada,canal):
-        if(entrada==1):
-            s = canal.get_sound()#s es el sonido que se esta reproduciendo
-            if s != None:
-               s.stop()#se detiene el sonido
-            sonido1 = pygame.mixer.Sound("parte01.wav")
-            sonido1.play()
-    
-    def capturarEntrada(self):
-        entrada = int(input("digite"))
-        return entrada
-      
+        self.eleccionAcontecimiento(sonido,sonido1,"parte01.wav",canal)
+        
+    def eleccionAcontecimiento(self,sonido,sonido1,titulo1,canal):
+        verificador = False
+        while not verificador:
+            for evento in pygame.event.get():
+                if evento.type == pygame.KEYDOWN:
+                    if evento.key == pygame.K_1 :
+                        self.detenerSonido(canal)
+                        acontecimiento1 = pygame.mixer.Sound(titulo1)
+                        canal.queue(acontecimiento1)
+                        verificador = True
+                        
+    def detenerSonido(self,canal):
+        sonido = canal.get_sound()
+        if sonido != None:
+            sonido.stop()
+            
     def procesoDelCuento(self):
         i=0
         while True:
             if(i==0):
                 self.iniciarCuento() 
                 i=i+1
-            for eventos in pygame.event.get():
-                if eventos.type == pygame.QUIT:
-                    exit()
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                    exit()          
+               
+                
