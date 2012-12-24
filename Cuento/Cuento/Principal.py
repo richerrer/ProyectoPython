612068bbbb77ae56ae7912416b3d1988051fe127
richerrer <ricardo_290792@hotmail.com>
2012-12-24 16:42:02 -0500

    agragando eleccion

--- Cuento/Cuento/Principal.py
+++ Cuento/Cuento/Principal.py
@@ -21,7 +21,7 @@ class Principal():
         canal.queue(opcion1)
         self.proceso_de_eleccion(canal)
     
-	def proceso_de_eleccion(self,canal):   
+    def proceso_de_eleccion(self,canal):   
         if self.eleccionAcontecimiento("atajo.wav","rodear_la_montana.wav","opcion_atajo.wav","opcion_rodear_montana.wav",canal)==1:
             #ATAJO
             if self.eleccionAcontecimiento("rodear_la_montana.wav","investigar_de_donde_vino_el_sonido.wav","opcion_rodear_montana.wav","opcion_de_investigar_de_donde_vino_el_sonido.wav",canal)==1:
@@ -34,7 +34,9 @@ class Principal():
                     if self.eleccionAcontecimiento("huir.wav","quedarse_acostado.wav","opciones_de_huir.wav","opciones_de_quedarse_acostado.wav",canal)==1:
                         #HUIR
                         self.eleccionAcontecimiento("letrero_naranja.wav","letrero_verde.wav","no existe opciones","no existe opciones",canal)        
-                    #FALTAAAAAAAAAAAAAAAAAAAA QUEDARSE ACOSTADO
+                    else:
+                        #QUEDARSE ACOSTADO
+                        self.eleccionAcontecimiento("golpear_al_alien.wav","letrero_verde.wav","no existe opciones","no existe opciones",canal)        
                 else:
                     #SALIR HUYENDO
                     self.eleccionAcontecimiento("atravesar_el_pantano.wav","subir_la_escalera.wav","no existe opciones","no existe opciones",canal)            
