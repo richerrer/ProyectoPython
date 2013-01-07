6f52f2ecb0a2bfd7a27fecd9726fcc3308fa6623
richerrer <ricardo_290792@hotmail.com>
2013-01-06 21:59:33 -0500

    terminando el archivo principal
    implemento todo el proceso en un solo archivo y agrego un proceso para
    finalizar el cuento

--- Cuento/Cuento/Principal.py
+++ Cuento/Cuento/Principal.py
@@ -1,6 +1,10 @@
 import pygame
-import Tkinter as tk
 from pygame.locals import *
+import os
+
+def main():
+    p=Principal()
+    
 
 class Principal():
     
@@ -15,40 +19,57 @@ class Principal():
         
     def iniciarCuento(self):
         canal = pygame.mixer.Channel(2)
-        sonido = pygame.mixer.Sound("inicio.wav")
-        opcion1 = pygame.mixer.Sound("opcion1.wav")
+        sonido = pygame.mixer.Sound(os.path.join("sonidos", "inicio.wav"))
+        opcion1 = pygame.mixer.Sound(os.path.join("sonidos", "opcion1.wav"))
         canal.queue(sonido)
         canal.queue(opcion1)
         self.proceso_de_eleccion(canal)
     
     def proceso_de_eleccion(self,canal):   
-        if self.eleccionAcontecimiento("atajo.wav","rodear_la_montana.wav","opcion_atajo.wav","opcion_rodear_montana.wav",canal)==1:
+        if self.eleccionAcontecimiento(os.path.join("sonidos", "atajo.wav"),os.path.join("sonidos", "rodear_la_montana.wav"),os.path.join("sonidos", "opcion_atajo.wav"),os.path.join("sonidos", "opcion_rodear_montana.wav"),canal)==1:
             #ATAJO
-            if self.eleccionAcontecimiento("rodear_la_montana.wav","investigar_de_donde_vino_el_sonido.wav","opcion_rodear_montana.wav","opcion_de_investigar_de_donde_vino_el_sonido.wav",canal)==1:
+            if self.eleccionAcontecimiento(os.path.join("sonidos", "rodear_la_montana.wav"),os.path.join("sonidos", "investigar_de_donde_vino_el_sonido.wav"),os.path.join("sonidos", "opcion_rodear_montana.wav"),os.path.join("sonidos", "opcion_de_investigar_de_donde_vino_el_sonido.wav"),canal)==1:
                 #RODEAR MONTANA
-                self.eleccionAcontecimiento("cruza_el_rio.wav","atraviesa_el_puente.wav","opcion_rodear_montana.wav","opcion_de_investigar_de_donde_vino_el_sonido.wav",canal)
+                self.ramas_de_rodear_montana(canal)
             else :
                 #INVESTIGAR DE DONDE VINO EL SONIDO
-                self.ramas_de_conclusion(canal)
+                self.ramas_de_entrar_en_la_nave(canal)
         else :
             #RODEAR MONTANA
-            self.eleccionAcontecimiento("cruza_el_rio.wav","atraviesa_el_puente.wav","no existe opciones","no existe opciones",canal)
+            self.ramas_de_rodear_montana(canal)
     
-    
-    def ramas_de_conclusion(self,canal):
-        if self.eleccionAcontecimiento("entrar_a_la_nave.wav","salir_huyendo.wav","opciones_de_entrar_a_la_nave.wav","opciones_de_salir_huyendo.wav",canal)==1:
+    def ramas_de_entrar_en_la_nave(self,canal): #UNA DE LAS RAMAS PRINCIPALES
+        if self.eleccionAcontecimiento(os.path.join("sonidos", "entrar_a_la_nave.wav"),os.path.join("sonidos", "salir_huyendo.wav"),os.path.join("sonidos", "opciones_de_entrar_a_la_nave.wav"),os.path.join("sonidos", "opciones_de_salir_huyendo.wav"),canal)==1:
             #ENTRAR A LA NAVE
-            if self.eleccionAcontecimiento("huir.wav","quedarse_acostado.wav","opciones_de_huir.wav","opciones_de_quedarse_acostado.wav",canal)==1:
+            if self.eleccionAcontecimiento(os.path.join("sonidos", "huir.wav"),os.path.join("sonidos", "quedarse_acostado.wav"),os.path.join("sonidos", "opciones_de_huir.wav"),os.path.join("sonidos", "opciones_de_quedarse_acostado.wav"),canal)==1:
                 #HUIR
-                self.eleccionAcontecimiento("letrero_naranja.wav","letrero_verde.wav","no existe opciones","no existe opciones",canal)        
+                self.eleccionAcontecimiento(os.path.join("sonidos", "letrero_naranja.wav"),os.path.join("sonidos", "letrero_verde.wav"),"no existe opciones","no existe opciones",canal)        
             else:
                 #QUEDARSE ACOSTADO
-                self.eleccionAcontecimiento("golpear_al_alien.wav","letrero_verde.wav","no existe opciones","no existe opciones",canal)        
+                self.eleccionAcontecimiento(os.path.join("sonidos", "golpear_al_alien.wav"),os.path.join("sonidos", "letrero_verde.wav"),"no existe opciones","no existe opciones",canal)        
         else:
             #SALIR HUYENDO
-            self.eleccionAcontecimiento("atravesar_el_pantano.wav","subir_la_escalera.wav","no existe opciones","no existe opciones",canal)            
+            self.eleccionAcontecimiento(os.path.join("sonidos", "atravesar_el_pantano.wav"),os.path.join("sonidos", "subir_la_escalera.wav"),"no existe opciones","no existe opciones",canal)            
         
     
+    def ramas_de_rodear_montana(self,canal):#OTRA DE LAS RAMAS PRINCIPALES QUE CONTIENE LA RAMA DE ENTRAR EN LA NAVE
+        if self.eleccionAcontecimiento(os.path.join("sonidos", "cruza_el_rio.wav"),os.path.join("sonidos", "atraviesa_el_puente.wav"),os.path.join("sonidos", "opciones_de_cruzar_nadando.wav"),os.path.join("sonidos", "opciones_de_cruzar_puente.wav"),canal)==1:
+            #CRUZAR EL RIO
+            if self.eleccionAcontecimiento(os.path.join("sonidos", "lo_golpea_una_piedra.wav"),os.path.join("sonidos", "no_lo_golpea_una_piedra.wav"),os.path.join("sonidos", "opcion_de_investigar_de_donde_vino_el_sonido.wav"),os.path.join("sonidos", "opcion_de_investigar_de_donde_vino_el_sonido.wav"),canal)==1:
+                #LO GOLPEA UNA PIEDRA
+                self.ramas_de_entrar_en_la_nave(canal)
+            else:
+                #NO LO GOLPEA UNA PIEDRA
+                self.ramas_de_entrar_en_la_nave(canal)
+        else:
+            #ATRAVIESA EL PUENTE
+            if self.eleccionAcontecimiento(os.path.join("sonidos", "cruza_el_puente.wav"),os.path.join("sonidos", "cae_del_puente.wav"),os.path.join("sonidos", "opcion_de_investigar_de_donde_vino_el_sonido.wav"),os.path.join("sonidos", "opcion_de_investigar_de_donde_vino_el_sonido.wav"),canal)==1:
+                #CRUZA EL PUENTE
+                self.ramas_de_entrar_en_la_nave(canal)
+            else:
+                #CAE DEL PUENTE
+                self.ramas_de_entrar_en_la_nave(canal)
+        
     def eleccionAcontecimiento(self,titulo1,titulo2,opcion_acontesimiento1,opcion_acontesimiento2,canal):
         verificador = False
         while not verificador:
@@ -69,6 +90,11 @@ class Principal():
                         self.verificar_existencia_opciones(opcion_acontesimiento2, canal)
                         verificador = True
                         r = 2
+                        
+                if evento.type == pygame.QUIT:
+                    exit()
+            
+                 
         return r
                 
     def verificar_existencia_opciones(self,opcion_acontesimiento,canal):
@@ -90,5 +116,8 @@ class Principal():
             for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
                     exit()          
-               
+
+
+
+main()
                 
